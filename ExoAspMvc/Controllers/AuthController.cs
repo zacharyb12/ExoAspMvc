4998023e3e90f62aea1c96172766d70382b65524
Zac <boualemzachary89@yahoo.fr>
2024-06-13 10:11:58 +0200

    Add project files.

---
+++ ExoAspMvc/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExoAspMvc.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExoAspMvc/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExoAspMvc.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
